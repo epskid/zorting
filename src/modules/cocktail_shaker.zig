e42f369fe2c1906341d47da8bb99c3771be9c3ba
chord <None>
2025-07-05 16:13:36 -0600

    optimize cocktail shaker sort

--- src/modules/cocktail_shaker.zig
+++ src/modules/cocktail_shaker.zig
@@ -1,37 +1,58 @@
 const array = @import("../array.zig");
+const std = @import("std");
 
 var index: usize = 0;
-var swapped = false;
 var backwards = false;
-var sorted: usize = 0;
+var swapped = false;
 
-pub fn init(_: *const array.Array) !void {
+var begin_index: usize = 0;
+var end_index: usize = 0;
+
+var new_begin_index: usize = 0;
+var new_end_index: usize = 0;
+
+pub fn init(arr: *const array.Array) !void {
     index = 0;
-    swapped = false;
     backwards = false;
-    sorted = 0;
+    swapped = false;
+
+    begin_index = 0;
+    end_index = arr.len - 1;
+
+    new_begin_index = end_index;
+    new_end_index = begin_index;
 }
 
 pub fn deinit() void {}
 
 pub fn step(arr: *array.Array) bool {
-    if (!backwards and index == arr.len - 1 - sorted) {
+    if (!backwards and index == end_index) {
         const done = !swapped;
         swapped = false;
+
         backwards = true;
         index -= 1;
+        end_index = new_end_index;
+
         return done;
-    } else if (backwards and index == sorted) {
+    } else if (backwards and index == begin_index) {
         const done = !swapped;
         swapped = false;
+
         backwards = false;
-        sorted += 1;
+        begin_index = new_begin_index;
+
         return done;
     }
 
     if (arr.get(index) > arr.get(index + 1)) {
         swapped = true;
         arr.swap(index, index + 1);
+        if (!backwards) {
+            new_end_index = index;
+        } else {
+            new_begin_index = index;
+        }
     }
     arr.comparisons += 1;
 
