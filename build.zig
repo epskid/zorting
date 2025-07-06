8e55571577f159a5ea4300c189df32c8acbd14b9
chord <None>
2025-07-05 20:27:14 -0600

    memory growth?

--- build.zig
+++ build.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const rlz = @import("raylib_zig");
+const emcc = @import("emcc.zig");
 
 pub fn build(b: *std.Build) !void {
     const target = b.standardTargetOptions(.{});
@@ -16,19 +17,19 @@ pub fn build(b: *std.Build) !void {
     const raylib_artifact = raylib_dep.artifact("raylib");
 
     if (target.query.os_tag == .emscripten) {
-        const exe_lib = try rlz.emcc.compileForEmscripten(b, "'zorting'", "src/main.zig", target, optimize);
+        const exe_lib = try emcc.compileForEmscripten(b, "'zorting'", "src/main.zig", target, optimize);
 
         exe_lib.linkLibrary(raylib_artifact);
         exe_lib.root_module.addImport("raylib", raylib);
         exe_lib.root_module.addImport("raygui", raygui);
 
-        const link_step = try rlz.emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
+        const link_step = try emcc.linkWithEmscripten(b, &[_]*std.Build.Step.Compile{ exe_lib, raylib_artifact });
         link_step.addArg("--emrun");
         link_step.addArg("--embed-file");
         link_step.addArg("resources/");
 
         b.getInstallStep().dependOn(&link_step.step);
-        const run_step = try rlz.emcc.emscriptenRunStep(b);
+        const run_step = try emcc.emscriptenRunStep(b);
         run_step.step.dependOn(&link_step.step);
         const run_option = b.step("run", "Run 'zorting'");
         run_option.dependOn(&run_step.step);
